46ae7534a80389b49cde418ce06ec67530bdac63
mo.azdd <102923017+llazdll@users.noreply.github.com>
2025-04-11 18:43:07 +0330

    implementig authentication

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  remotePatterns: [
+    {
+      protocol: "https",
+      hostname: "lh3.googleusercontent.com",
+    }
+  ]
 };
+module.exports = {
+  images: {
+    remotePatterns: [new URL('https://lh3.googleusercontent.com/a/ACg8ocKQ2CxF0AIlmRP5g_5mSYy_vsF0ej11-Hx304ySuPDHmv21BA=s96-c')],
+  },
+}
 
 export default nextConfig;
